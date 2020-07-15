e26340611090e58777e2fec245d84356e4a88152
Serhiy <sgvcool@gmail.com>
2020-07-15 16:02:46 +0300

    modules export, import

---
+++ 7_modules/index.js
@@ -0,0 +1,9 @@
+import Logger {COLOR, compute} from './module'
+
+//default export
+//Logger.log()
+
+// import all of module
+import * as Module from './module'
+
+Module.default.log()
---
+++ 7_modules/module.js
@@ -0,0 +1,11 @@
+export const COLOR = '#bababa'
+const privateLable = 43
+export function compute(a,b){
+    return a + b
+}
+
+export default{
+    log(){
+        console.log(privateLable)
+    }
+}
